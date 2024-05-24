4de93546a6ec2f0506811eec3644456273c53fdf
uedersilva <uedersilva@gmail.com>
2024-05-23 22:38:16 -0300

    estudando sobre tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Estudar tipos primitivos
+        //Estudar a classe String que representa texto na aplicação
+
+        String meuNome = "UEDER SILVA PINTO";
+
+        double salarioMinimo = 1412;
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+        
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.1546;
+
+        //VALOR_DE_PI = 10; VARIAVEL COM final NÃO PODE SER ALTERADA
     }
 }
